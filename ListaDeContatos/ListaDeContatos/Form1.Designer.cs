dc55fd7247ee595e46ba28988b87f8bad1711748
Sanmmmm <samngof@gmail.com>
2024-12-04 23:05:42 -0300

    Add files via upload

---
+++ ListaDeContatos/ListaDeContatos/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ListaDeContatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bradley Hand ITC", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOME:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bradley Hand ITC", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(87, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TELEFONE:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bradley Hand ITC", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(87, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "E-MAIL:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Bradley Hand ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(244, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(485, 32);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Bradley Hand ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(244, 98);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(485, 32);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Bradley Hand ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(244, 153);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(485, 32);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 48);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "ADICIONAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 48);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "EDITAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(322, 204);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 48);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "REMOVER";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(456, 204);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 48);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "LIMPAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Bradley Hand ITC", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(74, 258);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(655, 151);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bradley Hand ITC", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(275, -1);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(210, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "LISTA DE CONTATOS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ListaDeContatos/ListaDeContatos/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ListaDeContatos
+{
+    public partial class Form1 : Form
+    {
+        private List<Contato> contatos = new List<Contato>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Contato
+        {
+            public string Nome { get; set; }
+            public string Telefone { get; set; }
+            public string Email { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Validate input (optional, but recommended)
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Nome e telefone são obrigatórios!", "Aviso");
+                return;
+            }
+
+            // Create a new Contato object
+            Contato novoContato = new Contato
+            {
+                Nome = textBox1.Text,
+                Telefone = textBox2.Text,
+                Email = textBox3.Text
+            };
+
+            // Add the contact to the list
+            contatos.Add(novoContato);
+
+            // Update the ListBox with formatted contact information
+
+            listBox1.Items.Add($"{"NOME   : "}{   novoContato.Nome    } {"\tTEL   : "} {   novoContato.Telefone     } {"\tE-MAIL   : "}{   novoContato.Email}");
+
+            // Clear the input fields
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Check if an item is selected
+            if (listBox1.SelectedIndex >= 0)
+            {
+                // Get the index of the selected item
+                int indice = listBox1.SelectedIndex;
+
+                // Update contact information based on input fields
+                contatos[indice].Nome = textBox1.Text;
+                contatos[indice].Telefone = textBox2.Text;
+                contatos[indice].Email = textBox3.Text;
+
+                // Update the ListBox with the formatted contact information
+                listBox1.Items[indice] = $"{"NOME  : "}{contatos[indice].Nome} {"  \tTEL  : "} {contatos[indice].Telefone} {" \tE-MAIL: "} {contatos[indice].Email}";
+            }
+            else
+            {
+                MessageBox.Show("Selecione um contato para editar!", "Aviso");
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            // Check if an item is selected
+            if (listBox1.SelectedIndex >= 0)
+            {
+                // Get the index of the selected item
+                int indice = listBox1.SelectedIndex;
+
+                // Remove the contact from the list
+                contatos.RemoveAt(indice);
+
+                // Remove the item from the ListBox
+                listBox1.Items.RemoveAt(indice);
+            }
+            else
+            {
+                MessageBox.Show("Selecione um contato para remover!", "Aviso");
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Clear the input fields and ListBox
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            listBox1.Items.Clear();
+
+            // Clear the list of contacts
+            contatos.Clear();
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Check if an item is selected
+            if (listBox1.SelectedIndex >= 0)
+            {
+                // Get the index of the selected item
+                int indice = listBox1.SelectedIndex;
+
+                // Pre-fill the input fields with the selected contact information
+                textBox1.Text = contatos[indice].Nome;
+                textBox2.Text = contatos[indice].Telefone;
+                textBox3.Text = contatos[indice].Email;
+            }
+            else
+            {
+                // Clear the input fields if no item is selected
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+            }
+
+        }
+    
+    }
+}
